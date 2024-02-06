4ffbdbc5a5b42622b5cb61be63e59863e20941c9
JesusG <py3user22@github.com>
2024-02-06 00:46:18 -0600

    206 save2

--- django_proj206in/settings.py
+++ django_proj206in/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_proj206in.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_mysql206',
+        'USER': 'admin207',
+        'PASSWORD': '50k$tyvM@',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
