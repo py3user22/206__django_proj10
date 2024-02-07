4abd6d06f0b564166d03e17203640cde0a8a1e72
JesusG <py3user22@github.com>
2024-02-06 18:59:32 -0600

    206 save4

--- ll_app206/views.py
+++ ll_app206/views.py
@@ -1,17 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Menu
+from .models import Menu, Booking
 from django.core import serializers
-from .models import Booking
 from datetime import datetime
 import json
 from .forms import BookingForm
+from django.views.decorators.csrf import csrf_exempt
+
 
 def home(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
+
+
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = ""
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
+
 
 def book(request):
     form = BookingForm()
@@ -22,6 +37,35 @@ def book(request):
     context = {'form':form}
     return render(request, 'book.html', context)
 
+
+# lab3: step6, views.py >> bookings() function edits
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist == False:
+            booking = Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+
+    date = request.GET.get('date', datetime.today().date())
+
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+
+    return HttpResponse(booking_json, content_type='application/json')
+
+
+
+
+'''
 # Add code for the bookings() view > lab2: Step8
 # _______________________________ good output to reservations html 0206
 def bookings(request):
@@ -31,17 +75,4 @@ def bookings(request):
 
     return HttpResponse(booking_json, content_type='application/json')
 # _______________________________lab2: step8 end
-
-
-def menu(request):
-    menu_data = Menu.objects.all()
-    main_data = {"menu": menu_data}
-    return render(request, 'menu.html', {"menu": main_data})
-
-
-def display_menu_item(request, pk=None): 
-    if pk: 
-        menu_item = Menu.objects.get(pk=pk) 
-    else: 
-        menu_item = "" 
-    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
+'''
